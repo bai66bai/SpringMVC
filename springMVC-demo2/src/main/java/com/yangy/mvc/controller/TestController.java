87b4d8eaac47ac1b718a2386db5f0693dda7bf76
yangy <1355285815@qq.com>
2024-10-11 17:53:07 +0800

    学习Springmvc注解

--- springMVC-demo2/src/main/java/com/yangy/mvc/controller/TestController.java
+++ springMVC-demo2/src/main/java/com/yangy/mvc/controller/TestController.java
@@ -3,10 +3,21 @@ package com.yangy.mvc.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletRequest;
+import java.net.http.HttpRequest;
+
 @Controller
 public class TestController {
     @RequestMapping("/")
     public String index(){
+        return "index";
+    }
+
+    @RequestMapping("/testParam")
+    public String TestServletApi(HttpServletRequest request){
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        System.out.println(username + password);
         return "index";
     }
 }
